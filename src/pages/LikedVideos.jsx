4e2155c1749ee762b98f18efa1213eb3c4e46e38
itskk <kalpatarusethi34@gmail.com>
2025-08-25 12:51:15 +0530

    add LikedVideo feature

---
+++ src/pages/LikedVideos.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { getLikedVideos } from '../store/slices/likeSlice';
+import { GuestComponent, VideoList } from '../components';
+import { icons } from '../assets/icons';
+
+function LikedVideos() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [videos, setVideos] = useState([]);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getLikedVideos()).then((res) => {
+            setVideos(res.payload);
+            setIsLoading(false);
+        });
+    }, []);
+
+    const isHistoryEmpty = !isLoading && videos?.length < 1;
+
+  return <>
+    {!isHistoryEmpty && <VideoList videos={videos} loading={isLoading} />}
+    {isHistoryEmpty &&(
+        <GuestComponent 
+          title='Empty Liked Video'
+          subtitle="You have no previously Liked Videos."
+          icon={<span className='p-5'>{icons.Like}</span>}
+          guest={false}
+        />
+    )}
+  </>
+}
+
+export default LikedVideos;
