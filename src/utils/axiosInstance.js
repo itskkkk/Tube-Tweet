e2e01f556bb1294c7827793fb813976cb8d38390
itskk <kalpatarusethi34@gmail.com>
2025-10-06 18:34:58 +0530

    updates

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 
 export const axiosInstance = axios.create({
-    baseURL : "https://backend-project-t25d.onrender.com/api/v1",
+    baseURL : "https://backend-project-4hkf.onrender.com/api/v1",
     withCredentials : true,
 })
