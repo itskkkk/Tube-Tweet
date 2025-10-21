487a3c479b1c1c7bec9028c29c0ba70ce1ea15b2
itskk <kalpatarusethi34@gmail.com>
2025-10-21 20:48:07 +0530

    improvements

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Outlet } from 'react-router-dom'
 import { Bounce, ToastContainer, toast } from 'react-toastify'
-import loader from './assets/loader.gif'
+import thin_loading from "./assets/thin_loading.svg"
 import { getCurrentUser } from './store/slices/authSlice.js'
 
 
@@ -19,12 +19,13 @@ function App() {
 
   if(loading)
     return(
-      <div className='h-screen w-full overflow-y-auto dark:bg-black bg-white text-gray-500 dark:text-white'>
+      <div className='h-screen w-full overflow-y-auto bg-[#121212] text-white'>
         <div className='flex flex-col items-center justify-center mt-64'>
-            {/* <img src={loader} className='logo w-24' alt=" Loading..." /> */}
+            <img src={thin_loading} className='logo w-24' alt="Loading..." />
             <h1 className='text-3xl text-center mt-8 font-semibold'>
-              Loading....
+              Please wait.....
             </h1>
+            <h1 className='text-xl text-center mt-4'>Refresh the page if it takes too long</h1>
         </div>
       </div>
   );
